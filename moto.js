f67e4a752a477b12610f20b18d41ceeb0d8226e9
Tu Nombre <ezetello23@gmail.com>
2023-10-05 18:04:38 -0300

    responsive

--- moto.js
+++ moto.js
@@ -174,7 +174,7 @@ class Carrito {
     // Recorro producto por producto del carrito, y los dibujo en el HTML
     for (const producto of this.carrito) {
       divCarrito.innerHTML += `
-          <div class="productoCarrito">
+          <div class="productoCarrito col-md-4">
             <h3>${producto.nombre}</h3>
             <p>$${producto.precio}</p>
             <p>Cantidad: ${producto.cantidad}</p>
@@ -229,7 +229,7 @@ function cargarProductos(productos) {
   // Recorremos producto por producto y lo dibujamos en el HTML
   for (const producto of productos) {
     divProductos.innerHTML += `
-        <div class="producto">
+        <div class="producto col-md-4">
           <h3>${producto.nombre}</h3>
           <p class="precio">$${producto.precio}</p>
           <div class="imagen">
